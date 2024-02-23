c9ef5526984472af7868215311502ba6f8e2414d
ZacharyHsu <zachary14584@gmail.com>
2024-02-23 20:17:56 +0800

    練習 RWD
    練習 RWD :
    
    1. 主要熟悉HTML/CSS 相關語法
    
    2. 開發方式是先設計出網頁呈現畫面，再調整手機畫面呈現版本

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function openForm() {
+    document.getElementById("ct-form").style.display = "block";
+    document.getElementById("ct-form-back").style.display = "block";
+  }
+  
+function closeForm() {
+    document.getElementById("ct-form").style.display = "none";
+    document.getElementById("ct-form-back").style.display = "none";
+  }
